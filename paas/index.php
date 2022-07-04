31be32030367696d08c05bb41bd49b977815d80b
Karthikeyan.B <59909122+karthikeyan2002@users.noreply.github.com>
2022-07-04 09:40:04 +0530

    Create index.php
    About Me website

---
+++ paas/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Karthikeyan Balaji</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<table>
+		<tr>
+			<td style='width: 30%;'>
+				<img class = 'newappIcon' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBNsaRs8v01gWe9TkIkz7ACdFZyJadnpToBJ0UypuRpg&s'>
+			</td>
+			<td>
+				<h1 id = "message"><?php echo "Karthikeyan Balaji"; ?></h1>
+				<p class='description'></p>Student at<span class="blue"> Thiagarajar College of Engineering</span>.
+				<p class='description'></p>Know more <a href="about.html">about him </a><span> / <span><a herf="https://in.linkedin.com/in/karthikeyan-balaji-99a637190"> LinkedIn</a>
+			    <p class='description'></p><a href="https://www.google.com/search?q=karthikeyan+balaji+tce&oq=ka&aqs=chrome.0.69i59j69i57j0i131i433i512j0i512j46i199i465i512j69i60l3.944j0j7&sourceid=chrome&ie=UTF-8"> Google </a>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
